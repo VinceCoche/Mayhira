580682a7dd2c739a8367771a53eee169aa029ded
VinceCoche <104711864+VinceCoche@users.noreply.github.com>
2022-05-04 12:54:47 +0200

    Add files via upload

--- Root/includes/accueil/accueilsousscript.php
+++ Root/includes/accueil/accueilsousscript.php
@@ -1,44 +1,48 @@
 <script>
-    var accueilBase = 150;
+    var accueilBase = 100;
     var accueilFade = accueilBase*3;
     var accueilAnima = accueilBase*7;
     var accueilInterval = (accueilFade*2) + accueilAnima;
     var animateLeft = "35px";
-    var animateTop = "325px";
+    var animateTop = "370px";
 
 
-    
-    
-
-    // $(document).ready(data());
+    var array = <?php echo json_encode($accueilArray); ?>;
 
 
+    $(array).each(function(id= 0, msg) {
+        id++;
+        $("#accueilanim").text(msg);
+    });
+
+    var accueilAnim = function() {
+        
+        $("#accueilanim").fadeIn(accueilFade);
+        $("#accueilanim").animate({
+            left: "-=" + animateLeft, 
+            top: "+=" + animateTop,
+        }, {
+            duration: accueilAnima
+        });
+        $("#accueilanim").fadeOut(accueilFade);
+        $("#accueilanim").animate({
+            left: "+=" + animateLeft, 
+            top: "-=" + animateTop,
+        }, {
+            duration: 0
+        });
+    }
 
 
-    // var accueilAnim = function() {
-    //     $("#accueilanim").fadeIn(accueilFade);
-    //     $("#accueilanim").animate({
-    //             left: "-=" + animateLeft, 
-    //             top: "+=" + animateTop,
-    //         }, {
-    //             duration: accueilAnima
-    //         });
-    //     $("#accueilanim").fadeOut(accueilFade);
-    //     $("#accueilanim").animate({
-    //             left: "+=" + animateLeft, 
-    //             top: "-=" + animateTop,
-    //         }, {
-    //             duration: 0
-    //         });
-    // }
+    var accueilLoop = function() {
+        accueilAnim();
+        setTimeout(accueilAnim, accueilInterval);
+        accueilLoop();
+    }
 
-    // var accueilLoop = function() {
-    //     accueilAnim();
-    //     setTimeout(accueilAnim, accueilInterval);
-    //     accueilLoop();
-    // }
 
-    // $(document).ready(accueilLoop());
+    $(document).ready(accueilLoop());
+
 
 </script>
 
