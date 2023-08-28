cde307b1cbbbec8bd07942a0a47c1a06f8942c71
VinceCoche <104711864+VinceCoche@users.noreply.github.com>
2023-08-29 01:45:35 +0200

    Delete Root directory

--- Root/includes/common/allcommon.php
+++
@@ -1,6 +0,0 @@
-<?php include("includes/common/commonphp/header.php");?><!--  Pour le header -->
-<?php include("includes/common/commonphp/navbar.php");?><!--  Pour le menu latéral -->
-
-<!-- Pour les scripts Jquery-------------------------------- -->
-
-<?php include("includes/scripts/navbarscript.php");?>
--- Root/includes/common/commonphp/header.php
+++
@@ -1,7 +0,0 @@
-<header>
-    <a href="accueil.php" id="linkaccueil">
-        <h1>Mayhira</h1>
-        <h2>Médium Pure</h2>
-    </a>
-    
-</header>
